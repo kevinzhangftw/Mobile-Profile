0cb7bbb7ba9d98d1c3bfc796379f77d5778d072d
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2018-05-18 13:44:53 -0700

    testing with webview

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import React from 'react'
+import { StyleSheet, Text, View, Image, WebView } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
 
 export default class App extends React.Component {
   render() {
@@ -9,24 +18,19 @@ export default class App extends React.Component {
     }
     return (
       <View style={styles.container}>
-        <FlatList
+        <WebView
+          source={{uri: 'http://artonagroup.betaartona.com'}}
+          style={{marginTop: 20, width: 375}}
+        />
+        {/* <FlatList
             data={[{key: 'a'},{key: 'b'}]}
             renderItem={()=> <Image 
               source={pic} 
-              style={{width: 193, height: 110}}
+              style={{width: 300, height: 300}}
               />
             }
-        />
+        /> */}
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
